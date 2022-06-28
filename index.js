bb1c81301f0240fa9f0bd62b84f1729d2594bbdc
Ivis Orea <ivisorea@gmail.com>
2022-06-28 10:38:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json())
+app.use('*', (req, res) => {res.send('La Porra Backend')});
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
