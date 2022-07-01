07a7c1fdd17e5f4382e9ecae9f3fce73d89e0edd
Ivis Orea <ivisorea@gmail.com>
2022-07-01 09:12:31 +0200

    fix: exposed MongoDB URI

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-const uri = 'mongodb+srv://admin:gun-xwy8YXH5unh2ujh@cluster0.2vaoxlb.mongodb.net/la-porra?authSource=admin&replicaSet=atlas-131vds-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 
 try{
     const client = await mongoose.connect(process.env.MONGODB_URI);
